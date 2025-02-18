83dbafd1384cc051d6e2a35a7966b44c63321e3d
neiltanderson <neiltanderson22@gmail.com>
2025-02-18 16:57:15 -0600

    002 added files

--- workspace/asteroids/main.py
+++ workspace/asteroids/main.py
@@ -4,6 +4,7 @@
 import pygame
 pygame.init()
 from constants import *
+from player import *
 
 def main():
 
@@ -13,13 +14,25 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        dt = clock.get_time() / 1000
+
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
